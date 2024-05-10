075025f70ddf1f14bc28bd0ebdfaf7a8005f86ec
MajidUPay <majid.saleem@upayments.com>
2024-05-10 17:45:02 +0300

    Storybook deployment with GitHub action

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,5 +1,17 @@
+import { Global } from '@emotion/react';
+
+import { GlobalStyle } from '../src/shared/global';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [
+    (Story) => (
+      <>
+      <Global styles={GlobalStyle} />
+      <Story />
+      </>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {
